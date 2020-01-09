9109b91e5f4d9c9959d13a7aa57150a3a89e0b37
Prerna Garg <2016csb1050@iitrpr.ac.in>
2020-01-09 10:23:06 +0530

    voters working

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -12,6 +12,9 @@ contract Election{
 
 	mapping(uint => Candidate) public candidates;
 	// returns an empty candidate if not found, hence we need a count var
+
+	mapping(address => bool) public voters;
+
 	uint public candidateCount;
 
 	// Constructor
@@ -24,5 +27,17 @@ contract Election{
 	function addCandidate (string memory _name) private{
 		candidateCount++;
 		candidates[candidateCount] = Candidate(candidateCount, _name , 0);
+	}
+
+	function vote (uint _candidateId) public{
+		// not voted before
+		require(!voters[msg.sender]);
+
+		// valid candidate
+		require(_candidateId > 0 && _candidateId <= candidateCount);
+
+		voters[msg.sender] = true;
+
+		candidates[_candidateId].voteCount++;
 	}
 }
