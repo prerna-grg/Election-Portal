644d8edc263a749fa71093eb1b352f30b52b1705
Prerna Garg <2016csb1050@iitrpr.ac.in>
2020-01-12 11:00:01 +0530

    added a timer and hid election results till end

--- election/src/js/app.js
+++ election/src/js/app.js
@@ -1,3 +1,7 @@
+// who can vote who can not -- register to vote
+// start a timer for 24 hours
+// do not show the total count of candidates' votes until election is over.
+
 App = {
   web3Provider: null,
   contracts: {},
@@ -7,20 +11,6 @@ App = {
     return App.initWeb3();
   },
 
-/*
-  initWeb3: async function() {
-    if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
-      App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
-    } else {
-      // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-      web3 = new Web3(App.web3Provider);
-    }
-    return App.initContract();
-  },
-*/
 
   initWeb3: async function () {
     // Modern dapp browsers...
@@ -85,9 +75,11 @@ App = {
     var electionInstance;
     var loader = $("#loader");
     var content = $("#content");
+    var results = $("#results");
 
     loader.show();
     content.hide();
+    results.hide();
 
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
@@ -97,41 +89,88 @@ App = {
       }
     });
 
-    // Load contract data
-    App.contracts.Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.candidateCount();
-    }).then(function(candidateCount) {
-      var candidatesResults = $("#candidatesResults");
-      candidatesResults.empty();
+    // here start and end times are the hours at which the election begins and ends
+    var startTime = '10:00:00';
+    var endTime = '11:00:00';
+    currentDate = new Date()
+    startDate = new Date(currentDate.getTime());
+    startDate.setHours(startTime.split(":")[0]);
+    startDate.setMinutes(startTime.split(":")[1]);
+    startDate.setSeconds(startTime.split(":")[2]);
+    endDate = new Date(currentDate.getTime());
+    endDate.setHours(endTime.split(":")[0]);
+    endDate.setMinutes(endTime.split(":")[1]);
+    endDate.setSeconds(endTime.split(":")[2]);
+    var valid = startDate < currentDate && endDate > currentDate;
 
-      var candidatesSelect = $("#candidatesSelect");
-      candidatesSelect.empty();
-      
-      for (var i = 1; i <= candidateCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
-          var id = candidate[0];
-          var name = candidate[1];
-          var voteCount = candidate[2];
+    if (valid){
+      // Load contract data
+      App.contracts.Election.deployed().then(function(instance) {
+        electionInstance = instance;
+        return electionInstance.candidateCount();
+      }).then(function(candidateCount) {
+        var candidatesResults = $("#candidatesResults");
+        candidatesResults.empty();
 
-          // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
-          candidatesResults.append(candidateTemplate);
+        var candidatesSelect = $("#candidatesSelect");
+        candidatesSelect.empty();
+        
+        for (var i = 1; i <= candidateCount; i++) {
+          electionInstance.candidates(i).then(function(candidate) {
+            var id = candidate[0];
+            var name = candidate[1];
+            var voteCount = candidate[2];
 
-          var candidateOption = "<option value='" + id + "'>" + name + "</option>";
-          candidatesSelect.append(candidateOption);
-        });
-      }
-      return electionInstance.voters(App.account);
-    }).then(function(hasVoted){
-      if(hasVoted){
-        $('form').hide();
-      }
-      loader.hide();
-      content.show();
-    }).catch(function(error) {
-      console.warn(error);
-    });
+            // Render candidate Result
+            var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td></tr>";
+            candidatesResults.append(candidateTemplate);
+
+            var candidateOption = "<option value='" + id + "'>" + name + "</option>";
+            candidatesSelect.append(candidateOption);
+          });
+        }
+        return electionInstance.voters(App.account);
+      }).then(function(hasVoted){
+        var voteMessage = $("#voteMessage");
+        voteMessage.empty();
+        if(hasVoted){
+          $('form').hide();
+          voteMessage.append("Thanks for voting. The results will be available at " + endTime );
+        }else{
+          voteMessage.append("The elections will end at " + endTime );
+        }
+        loader.hide();
+        content.show();
+      }).catch(function(error) {
+        console.warn(error);
+      });
+    }else{
+      // Load contract data
+      App.contracts.Election.deployed().then(function(instance) {
+        electionInstance = instance;
+        return electionInstance.candidateCount();
+      }).then(function(candidateCount) {
+        var finalResults = $("#finalResults");
+        finalResults.empty();
+
+        for (var i = 1; i <= candidateCount; i++) {
+          electionInstance.candidates(i).then(function(candidate) {
+            var id = candidate[0];
+            var name = candidate[1];
+            var voteCount = candidate[2];
+
+            // Render candidate Result
+            var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
+            finalResults.append(candidateTemplate);
+          });
+        }
+        loader.hide();
+        content.hide();
+        results.show();
+      }).catch(function(error) {
+        console.warn(error);
+      });
+    }
   },
 
 castVote: function() {
